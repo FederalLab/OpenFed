53b1874707f29e69588c28acce3b1a05bed3983e
densechen <densechen@foxmail.com>
2021-06-20 21:19:49 +0800

    结构整理

---
+++ openfed/helper.py
@@ -0,0 +1,12 @@
+import openfed
+import argparse
+import os
+import cmd
+
+
+def main():
+    print("I am here")
+
+
+if __name__ == '__main__':
+    print(os.getcwd())
--- openfed/utils/helper.py
+++
@@ -1,1 +0,0 @@
-# Provide a script to dynamic edit address.
