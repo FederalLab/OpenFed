f05894e4cef6a6642be4c969aefcf0d59052df1f
densechen <densechen@foxmail.com>
2021-06-22 22:26:08 +0800

    update

--- openfed/utils/keyboard_interrupt_handle.py
+++ openfed/utils/keyboard_interrupt_handle.py
@@ -9,13 +9,13 @@ def keyboard_interrupt_handle():
     """
     old_excepthook = sys.excepthook
 
-    def new_hook(type, value, traceback):
+    def keyboard_interrupt_hook(type, value, traceback):
         if type != KeyboardInterrupt:
             old_excepthook(type, value, traceback)
         else:
-            logger.info("Keyboard interrupt captured by OpenFed.")
+            logger.info("Keyboard Interrupt")
             logger.warning("Force Quit!")
             Destroy.destroy_all_in_all_world()
             logger.info("Exited.")
             old_excepthook(type, value, traceback)
-    sys.excepthook = new_hook
+    sys.excepthook = keyboard_interrupt_hook
