d72590c69a5c113ab9dd6397e894403434bbc455
densechen <densechen@foxmail.com>
2021-06-24 14:32:05 +0800

    update

--- demo.py
+++ demo.py
@@ -7,23 +7,15 @@ import torch.optim as optim
 
 import openfed
 import openfed.aggregate as aggregate
-from openfed.optim.elastic_aux import ElasticAux
 
 parser = openfed.parser
 args = parser.parse_args()
-
-# set cuda device
-# torch.cuda.set_device(args.rank % (torch.cuda.device_count()))
-# torch.cuda.set_device(0)
 
 # specify a api type.
 openfed_api = openfed.API(frontend=args.rank > 0)
 
 # build the connection
 openfed_api.build_connection(address=openfed.Address(args=args))
-
-# show the digest of current world
-print(openfed_api.openfed_digest())
 
 # build a network
 net = nn.Linear(1, 1)
@@ -33,12 +25,8 @@ net = nn.Linear(1, 1)
 optimizer = optim.SGD(net.parameters(), lr=0.1, momentum=0.9)
 
 # define aggregator (actually, this is only used for server end)
-aggregator = aggregate.ElasticAggregator(
+aggregator = aggregate.AverageAggregator(
     net.parameters(), other_keys="momentum_buffer")
-
-# define aggregator aux (actually, this is only used for client end)
-# some federated algorithms need to do extra computation on client.
-elastic_aux = ElasticAux(net.parameters())
 
 # tell openfed_api which part of data is needed to be cared.
 openfed_api.set_state_dict(net.state_dict(keep_vars=True))
@@ -63,16 +51,13 @@ with openfed_api:
 
         # reset
         optimizer.zero_grad()
-        elastic_aux.clear_buffer()
 
         # training
         net(torch.randn(128, 1, 1)).sum().backward()
-        elastic_aux.step()
         optimizer.step()
 
         # submit
         openfed_api.pack_state(optimizer, keys=['momentum_buffer'])
-        openfed_api.pack_state(elastic_aux)
 
         openfed_api.set_task_info({"train_instances": random.randint(1, 200)})
 
