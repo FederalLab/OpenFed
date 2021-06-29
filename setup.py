7e0b712ee5469873c1a5bd81e3c785a85e15aa7c
densechen <densechen@foxmail.com>
2021-06-29 16:31:47 +0800

    fix error

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10,"
+        "Programming Language :: Python :: 3.10",
         "Topic :: Documentation :: Sphinx",
         "Topic :: Scientific/Engineering :: Artificial Intelligence",
         "Topic :: Scientific/Engineering :: Image Processing",
