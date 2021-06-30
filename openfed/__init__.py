3b1a5c9b4a78f5a26c152754eb28bbcbb82a3cba
densechen <densechen@foxmail.com>
2021-06-30 14:51:41 +0800

    update

--- openfed/__init__.py
+++ openfed/__init__.py
@@ -3,7 +3,7 @@ __version__ = "0.0.0"
 
 from .api import API
 from .common import *
-from .unified.step.at_last import (AggregateCount, AggregatePeriod,
+from .unified.step import (AggregateCount, AggregatePeriod,
                                    StopAtLoopTimes, StopAtVersion)
 
 del api
