5162d4e11ea76d1d623fd118b23954d0976f2fe5
densechen <densechen@foxmail.com>
2021-07-04 04:06:21 +0800

    vars

--- openfed/common/vars.py
+++ openfed/common/vars.py
@@ -26,66 +26,59 @@ from openfed.utils import openfed_class_fmt
 
 class Vars(object):
     flag: bool
+    class_name: str
 
-    def __init__(self):
-        self.flag = False
+    def __init__(self, flag: bool, class_name: str):
+        self.flag = flag
+        self.class_name = class_name
 
     def set(self, flag: bool):
         self.flag = flag
 
-    def __repr__(self):
+    def __str__(self):
         return openfed_class_fmt.format(
-            class_name="Vars",
-            description="Base class for global variables."
+            class_name=self.class_name,
+            description=f"Flag: {self.flag}."
         )
 
 
 class _DAL(Vars):
+    """Dynamic Address Loading.
+    """
+
     def __init__(self):
-        self.flag = True
+        super().__init__(True, 'DynamicAddressLoading')
 
-    def set_dal(self):
-        self.flag = True
+    def set_dal(self) -> None:
+        self.set(True)
 
-    def unset_dal(self):
-        self.flag = False
+    def unset_dal(self) -> None:
+        self.set(False)
 
     @property
     def is_dal(self) -> bool:
         return self.flag
-
-    def __repr__(self) -> str:
-        return openfed_class_fmt.format(
-            class_name="DAL",
-            description="If dal is enabled, a thread will be created to maintain new connection."
-                        "Otherwise, OpenFed will get stuck until all address are correctly jointed."
-                        "Call set_dal() to set dal, unset_dal to disable it."
-        )
 
 
 DAL = _DAL()
 
 
 class _ASYNC_OP(Vars):
+    """ASYNC OP.
+    """
+
     def __init__(self):
-        self.flag = True
+        super().__init__(True, "ASYNC_OP")
 
-    def set_async_op(self):
-        self.flag = True
+    def set_async_op(self) -> None:
+        self.set(True)
 
-    def unset_async_op(self):
-        self.flag = False
+    def unset_async_op(self) -> None:
+        self.set(False)
 
     @property
     def is_async_op(self) -> bool:
         return self.flag
-
-    def __repr__(self) -> str:
-        return openfed_class_fmt.format(
-            class_name="ASYNC_OP",
-            description="If True, the download and upload operation will return an handler."
-                        "Otherwise, it will be blocked until finish."
-        )
 
 
 ASYNC_OP = _ASYNC_OP()
