e01234ae4eefa08d7c58b65fdf4d5224c0ae4c0d
densechen <densechen@foxmail.com>
2021-07-05 17:59:39 +0800

    update

--- tests/api/multi/test_dispatch.py
+++ tests/api/multi/test_dispatch.py
@@ -2,5 +2,5 @@ from openfed.api.multi.dispatch import Dispatch
 
 
 def test_dispatch():
-    Dispatch(total_parts=1, samples=1)
-    Dispatch(total_parts=1, sample_ratio=1)
+    Dispatch(1, samples=1)
+    Dispatch(1, sample_ratio=1)
