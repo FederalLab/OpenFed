d447e83a5f2c438df6cb3aa21d16024d9c37d856
densechen <densechen@foxmail.com>
2021-07-10 10:19:25 +0800

    update

--- openfed/api/api.py
+++ openfed/api/api.py
@@ -319,15 +319,15 @@ class API(SafeThread, Hook):
                     # update regularly.
                     step(at_last)
 
-            try_times = 0 if cnt else try_times + 1
+            # try_times = 0 if cnt else try_times + 1
 
-            if cnt == 0:
-                logger.info(
-                    f"Empty reign, waiting {try_times}/{self.max_try_times}...")
-                time.sleep(5.0)
+            # if cnt == 0:
+            #     logger.info(
+            #         f"Empty reign, waiting {try_times}/{self.max_try_times}...")
+            #     time.sleep(5.0)
 
-            if try_times >= self.max_try_times:
-                self.manual_stop()
+            # if try_times >= self.max_try_times:
+            #     self.manual_stop()
 
             # left some time to maintainer lock
             time.sleep(0.1)
