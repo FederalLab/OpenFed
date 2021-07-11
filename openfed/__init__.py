ea0c1369ca2ebd85ee7034c88ad94b13bc79dc08
densechen <densechen@foxmail.com>
2021-07-11 15:15:02 +0800

    fix dockerfile

--- openfed/__init__.py
+++ openfed/__init__.py
@@ -1,13 +1,13 @@
 # Refer [here](https://semver.org/) to learn more about Semantic Versioning Specification.
 __version__ = "0.0.0"
 
-import openfed.api
-import openfed.common
+import openfed.api as fed_api
+import openfed.container as fed_container
 import openfed.core
-import openfed.data
-import openfed.optim
-import openfed.pipe
-import openfed.topo
+import openfed.data as fed_data
+import openfed.optim as fed_optim
+import openfed.pipe as fed_pipe
+import openfed.topo as fed_topo
 import openfed.utils
 
 from .api import API
