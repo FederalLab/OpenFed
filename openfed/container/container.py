14f3d4081c1e29bdee327c1d2ee5a0eb991cc851
densechen <densechen@foxmail.com>
2021-07-13 16:30:46 +0800

    fix error

--- openfed/container/container.py
+++ openfed/container/container.py
@@ -7,7 +7,10 @@ from .reducer import Reducer
 class Container(Agg, Reducer):
     """Glue Agg and Reducer into a single class, named Container.
     """
+    def __new__(cls):
+        raise RuntimeError("Call `build_container` to create a container.")
 
 
-def build_container(aggregator: Agg, reducer: Reducer) -> Container:
+def build_container(aggregator: Agg, reducer: Reducer = None) -> Container:
+    reducer = Reducer() if reducer is None else reducer
     return glue(aggregator, reducer)
--- openfed/pipe/pipe.py
+++ openfed/pipe/pipe.py
@@ -6,8 +6,10 @@ from openfed.utils import glue
 class Pipe(Optimizer, Penalizer):
     """Glue Optimizer and Penalizer as a single class.
     """
-    ...
+    def __new__(cls):
+        raise RuntimeError("Call `build_pipe` to create a pipe.")
 
 
-def build_pipe(optimizer: Optimizer, penalizer: Penalizer) -> Pipe:
+def build_pipe(optimizer: Optimizer, penalizer: Penalizer = None) -> Pipe:
+    penalizer = Penalizer() if penalizer is None else penalizer
     return glue(optimizer, penalizer)
