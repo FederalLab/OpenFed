dc6af77180d4f8adba25bf3126674f041e71139d
densechen <densechen@foxmail.com>
2021-08-26 21:14:38 +0800

    add topo

--- openfed/topo/__init__.py
+++ openfed/topo/__init__.py
@@ -1,15 +1,5 @@
-from .centralized import CentralizedTopo
-from .decentralized import DecentralizedTopo
-from .hierarchical import HierarchicalTopo
-from .split import SplitTopo
 from .topo import Topology
-from .vertical import VerticalTopo
 
 __all__ = [
-    'CentralizedTopo',
-    'DecentralizedTopo',
-    'HierarchicalTopo',
-    'VerticalTopo',
-    'SplitTopo',
     'Topology',
 ]
