00f88bf7b969689f467c873e4d16c0340a474202
densechen <densechen@foxmail.com>
2021-09-02 23:55:12 +0800

    doc

--- openfed/core/federated.py
+++ openfed/core/federated.py
@@ -1,4 +1,5 @@
 # Copyright (c) FederalLab. All rights reserved.
+from datetime import timedelta
 from threading import Lock
 from typing import Any, Dict, List, Optional, Tuple
 
@@ -7,12 +8,24 @@ from openfed.common import Address
 from openfed.utils import openfed_class_fmt, tablist
 
 from .const import follower_rank, leader_rank
-from datetime import timedelta
 
 default_pg_timeout = timedelta(seconds=100)
 
 
 class DistributedProperties(object):
+    r"""Keeps all distributed properties in this class, so that we can build
+    multi-process groups.
+
+    Args:
+        lock: A lock used to protect distributed properties. Default: ``None``
+    
+    .. Example::
+
+        >>> dist_props = DistributedProperties()
+        >>> dist_props
+        <OpenFed> DistributedProperties
+        GroupCount: 0
+    """
     # Use to save the default distributed probabilities
     _default_WORLD = distributed_c10d.group.WORLD
     _default_pg_map = distributed_c10d._pg_map
@@ -92,8 +105,58 @@ class DistributedProperties(object):
             description=f"GroupCount: {self._group_count}")
 
 
+class FederatedProperties(object):
+    r"""Keeps all federated properties in this class, so that we can build
+    multi-process groups.
+
+    Args:
+        role: The role played.
+        nick_name: The name of node.
+        address: The address to connect this federated group.
+
+    Examples::
+
+        >>> fed_props = FederatedProperties('leader', 'openfed_node', default_tcp_address)
+        >>> fed_props
+        <OpenFed> FederatedProperties
+        +--------+--------------+
+        |  role  |  nick_name   |
+        +--------+--------------+
+        | leader | openfed_node |
+        +--------+--------------+
+        <OpenFed> Address
+        +---------+---------------------+------------+------+
+        | backend |     init_method     | world_size | rank |
+        +---------+---------------------+------------+------+
+        |   gloo  | tcp://localhost:... |     2      |  -1  |
+        +---------+---------------------+------------+------+
+    """
+    role: str
+    nick_name: str
+    address: Address
+
+    def __init__(self, role: str, nick_name: str, address: Address):
+        self.role = role
+        self.nick_name = nick_name
+        self.address = address
+
+    def __repr__(self):
+        head = ['role', 'nick_name']
+        data = [self.role, self.nick_name]
+        description = tablist(head, data, force_in_one_row=True)
+        other_description = str(self.address)
+        return openfed_class_fmt.format(class_name=self.__class__.__name__,
+                                        description=description + "\n" +
+                                        other_description)
+
+
 def build_point2point_group(
         rank: int = 0) -> List[distributed_c10d.ProcessGroup]:
+    r"""Builds process groups between two ranks.
+
+    Args:
+        rank: The rank of the target process group. Default: ``0``
+    """
     if distributed_c10d.get_world_size() == 2:
         return [distributed_c10d._get_default_group()]
 
@@ -109,12 +172,36 @@ def build_point2point_group(
     return pg_list
 
 
-def joint_federated_group(
-    backend,
-    init_method=None,
-    world_size=-1,
-    rank=-1,
-):
+def joint_federated_group(backend,
+                          init_method=None,
+                          world_size=-1,
+                          rank=-1) -> List[distributed_c10d.ProcessGroup]:
+    r"""Joints federated group. We will build a store manually used for meta
+    exchange.
+
+    .. warning ::
+        ``env://`` is not allowed to be used to avoid conflicts with distributed
+        learning in `PyTorch`.
+
+    Args:
+        backend: The backend to used. Depending on build-time configurations,
+            valid values include ``mpi``, ``gloo`` and  ``nccl``, which depends
+            on the `PyTorch` you installed. This field should be given as a
+            lowercase string (e.g., ``gloo``), which can also be accessed via
+            :class:`Backend` attributes (e.g., ``Backend.GLOO``). ``nccl`` is 
+            not recommended currently, for that we will always move the tensor 
+            to `cpu` first before sending to other nodes to avoiding device 
+            disalignment between `cpu` and `gpu`. Thus, ``nccl`` will not speed 
+            up the communication phase in `OpenFed`. Default: ``'gloo'``.
+        init_method: URL specifying how to initialize the federated group. Such
+            as: ``tcp://localhost:1994``, ``file:///tmp/sharefile``. If you use 
+            ``file://``, make sure the file is not existing. 
+            Default: ``'tcp://localhost:1994'``
+        world_size: Number of nodes in federated group. Default: ``2``
+        rank: Rank of current node (it should be a number between 0 and 
+            ``world_size``-1). If `-1` is provided, rank will be specified during
+            runtime. Default: -1
+    """
     # build a store
     rendezvous_iterator = distributed_c10d.rendezvous(
         init_method, rank, world_size, timeout=default_pg_timeout)
@@ -134,23 +221,3 @@ def joint_federated_group(
         leader_rank if distributed_c10d.get_world_size() == 2 else 0)
 
     return sub_pg_list
-
-
-class FederatedProperties(object):
-    role: str
-    nick_name: str
-    address: Address
-
-    def __init__(self, role: str, nick_name: str, address: Address):
-        self.role = role
-        self.nick_name = nick_name
-        self.address = address
-
-    def __repr__(self):
-        head = ['role', 'nick_name']
-        data = [self.role, self.nick_name]
-        description = tablist(head, data, force_in_one_row=True)
-        other_description = str(self.address)
-        return openfed_class_fmt.format(class_name=self.__class__.__name__,
-                                        description=description + "\n" +
-                                        other_description)
