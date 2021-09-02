0b90cd24a5e8143adf91f8a58b15c4aff38f1fd8
densechen <densechen@foxmail.com>
2021-09-03 00:06:13 +0800

    doc

--- openfed/optim/__init__.py
+++ openfed/optim/__init__.py
@@ -3,3 +3,10 @@ from .elastic import ElasticOptimizer
 from .fed_optim import FederatedOptimizer
 from .prox import ProxOptimizer
 from .scaffold import ScaffoldOptimizer
+
+__all__ = [
+    'ElasticOptimizer',
+    'FederatedOptimizer',
+    'ProxOptimizer',
+    'ScaffoldOptimizer',
+]
