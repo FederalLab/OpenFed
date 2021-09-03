38d37bcbbbe72ca8c5e60578ebd64efe5ae68480
densechen <densechen@foxmail.com>
2021-09-03 23:38:39 +0800

    update

--- openfed/data/__init__.py
+++ openfed/data/__init__.py
@@ -1,5 +1,5 @@
 # Copyright (c) FederalLab. All rights reserved.
-from .analysis import digest, label_distribution
+from .analysis import *
 from .datasets import FederatedDataset, PartitionerDataset
 from .partitioner import (DirichletPartitioner, IIDPartitioner, Partitioner,
                           PowerLawPartitioner)
