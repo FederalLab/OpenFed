ace5451d4fecebb9c3d9cc8e7cb4b806ffe07afa
densechen <densechen@foxmail.com>
2021-09-19 21:20:42 +0800

    fix import error

--- openfed/__init__.py
+++ openfed/__init__.py
@@ -2,7 +2,7 @@
 from openfed import core as core
 from openfed import data as data
 from openfed import federated as federated
-from openfed import functional as F
+from openfed import functional as functional
 from openfed import optim as optim
 from openfed import topo as topo
 from openfed.api import API as API
